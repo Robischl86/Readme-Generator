6ec2eb79a01a2d5605f4ab0d14d875704e838c24
Levi <levi.robischon@gmail.com>
2020-06-03 14:40:01 -0700

    added node questions

--- Levi's_Readme_Generator.md
+++
@@ -1,7 +0,0 @@
-{
-	"title": "Levi's Readme Generator",
-	"description": "This is a readme generator",
-	"screenshot": "Link",
-	"username": "Robischl",
-	"email": "levi.robischon@gmail.com"
-}
--- script.js
+++ script.js
@@ -28,23 +28,142 @@ inquirer
       type: "input",
       message: "Email: ",
       name: "email"
+    },
+    {
+      type: "input",
+      message: "Installation: ",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "Usage: ",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "License: ",
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "Contributors: ",
+      name: "contributors"
+    },
+    {
+      type: "input",
+      message: "Tests: ",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Questions: ",
+      name: "questions"
     }
+
   ])
   .then(function(response) {
     console.log(response);
 
+    let projectTitle = JSON.stringify(response.title);
+    projectTitle.id = "projectTitle";
+
+    let description = JSON.stringify(response.description);
+    description.id = "description";
+
+    let screenshot = JSON.stringify(response.screenshot);
+    console.log(screenshot)
+
+    let username = JSON.stringify(response.username);
+    username.id = "username";
+
+    let email = JSON.stringify(response.email);
+    email.id = "email"
+
+    let installation = JSON.stringify(response.installation);
+
+    let usage = JSON.stringify(response.usage);
+
+    let license = JSON.stringify(response.license);
+
+    let contributors = JSON.stringify(response.contributors);
+
+    let tests = JSON.stringify(response.tests);
+
+    let questions = JSON.stringify(response.questions);
+
+
+
+    console.log(projectTitle);
+    console.log(description);
+    console.log(username);
+    console.log(email);
+
+
     let filename = response.title.split(' ').join('_') + ".md";
 
-    fs.writeFile(filename, JSON.stringify(response, null, '\t'), function (err) {
-        
+
+
+    fs.writeFile(filename, ("## " + projectTitle + `\n`), function (err) {
       if (err) {
         return console.log(err);
       }
-
       console.log("Success!");
-      
     });
+//////////////////////////////////////////////
 
+    fs.appendFileSync(filename, ("This project was created by " + username + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## " + description + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("<img src=screenshot />")), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## Installation: " + installation + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## Usage: " + usage + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## License: " + license + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## Contributors: " + contributors + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## Tests: " + tests + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
+
+    fs.appendFileSync(filename, ("## Questions: " + questions + `\n`)), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    }
 
   });
 
